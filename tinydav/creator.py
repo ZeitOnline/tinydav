f0079d4bee0bc92d8d0a0c731040ffb5dc227923
Ron Drongowski <ron.drongowski@gmail.com>
2016-09-13 14:06:01 +0200

    Remove logic bug in proppatch for prop set

--- tinydav/creator.py
+++ tinydav/creator.py
@@ -101,10 +101,10 @@ def create_proppatch(setprops, delprops, namespaces=None):
     # <!ELEMENT set (prop) >
     if setprops:
         set_ = SubElement(propertyupdate, "set")
-        prop = SubElement(set_, "prop")
+        prop_el = SubElement(set_, "prop")
         items_iterator = setprops.iteritems() if PYTHON2 else setprops.items()
         for (propname, propvalue) in items_iterator:
-            prop = SubElement(prop, propname)
+            prop = SubElement(prop_el, propname)
             prop.text = propvalue
     # RFC 2518, 12.13.1 set XML element
     # <!ELEMENT remove (prop) >

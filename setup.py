77e20684f935aa1265f4a0da20824da516e1471d
Ron Drongowski <ron.drongowski@gmail.com>
2016-09-13 14:32:57 +0200

    Prepare initial release

--- setup.py
+++ setup.py
@@ -1,59 +1,25 @@
-#!/usr/bin/python
-# coding: utf-8
-#
-# Copyright (C) 2009  Manuel Hermann <manuel-hermann@gmx.net>
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-import tinydav
-
-DOWNLOAD = "http://tinydav.googlecode.com/files/tinydav-%s.tar.gz" % VERSION
-DESCRIPTION = "An easy-to-use HTTP and WebDAV client library."
-LONG_DESCRIPTION = """\
-An easy-to-use HTML and WebDAV client library for Python
---------------------------------------------------------
-
-This is a small library for contacting HTTP and WebDAV servers. Goal of this project until version 1.0 is supporting all WebDAV methods including the versioning extensions from RFC 3253).
-
-Features:
- - HTTP methods OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT.
- - WebDAV methods MKCOL, PROPFIND, PROPPATCH, DELETE, COPY, MOVE, LOCK, UNLOCK
- - Support for REPORT method (report-tree requests only, RFC 3253)
- - Cookies
- - SSL
- - Multipart/form-data and application/x-www-form-urlencoded POST requests
-
-This version requires Python 2.5 or later (including Python 3.)
-"""
-
-CLASSIFIERS = [
-    "Programming Language :: Python",
-    "Programming Language :: Python :: 2.5",
-    "Programming Language :: Python :: 2.6",
-    "Programming Language :: Python :: 2.7",
-    "Programming Language :: Python :: 3",
-    "Development Status :: 4 - Beta",
-    "Environment :: Web Environment",
-    "Intended Audience :: Developers",
-    "License :: OSI Approved :: "\
-        "GNU Library or Lesser General Public License (LGPL)",
-    "Operating System :: OS Independent",
-    "Topic :: Internet :: WWW/HTTP",
-    "Topic :: Software Development :: Libraries :: Python Modules",
-]
-
-if __name__ == "__main__":
-    setup(
-        name="tinydav",
-        packages=["tinydav"],
-        version='0.7.0+patchmultipleprops',
-        description=DESCRIPTION,
-        long_description=LONG_DESCRIPTION,
-        author="Manuel Hermann",
-        author_email="manuel-hermann@gmx.net",
-        url="http://code.google.com/p/tinydav/",
-        download_url=DOWNLOAD,
-        keywords = ["http", "https", "webdav", "library", "client"],
-        license="LGPL",
-        classifiers=CLASSIFIERS,
-    )
-
+setup(
+    name='tinydav',
+    url='https://github.com/ZeitOnline/tinydav',
+    version='0.7.0dev',
+    author='Manuel Hermann, Zeit Online',
+    author_email='zon-backend@zeit.de',
+    description='A small and handy WebDav Client',
+    long_description=open('README.txt', 'r').read(),
+    setup_requires=['setuptools_git'],
+    namespace_packages=['tinydav'],
+    include_package_data=True,
+    zip_safe=False,
+    keywords='WebDAV',
+    license='LGPL',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: Other/Proprietary License',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet ::'
+    ]
+)
